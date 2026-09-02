22229ea2600874e1b48dd2038b45174641008d07
webstean@gmail.com <(Get-Item Env:UPN).Value>
2026-09-02 17:08:30 +1000

    refactor: Rename role assignment resources for clarity

--- global-essential-machine-manager.tf
+++ global-essential-machine-manager.tf
@@ -5,14 +5,14 @@ resource "azurerm_role_assignment" "essential_machine_management_administrator" 
   description          = local.iac_message
 }
 
-resource "azurerm_role_assignment" "managed_identity_operator" {
+resource "azurerm_role_assignment" "essential_machine_management_managed_identity_operator" {
   scope                = "/subscriptions/${var.subscription_id}"
   role_definition_name = "Managed Identity Operator"
   principal_id         = var.owner_entra_object_id
   description          = local.iac_message
 }
 
-resource "azurerm_role_assignment" "resource_policy_contributor" {
+resource "azurerm_role_assignment" "essential_machine_management_resource_policy_contributor" {
   scope                = "/subscriptions/${var.subscription_id}"
   role_definition_name = "Resource Policy Contributor"
   principal_id         = var.owner_entra_object_id
