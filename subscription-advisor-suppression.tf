5c5195c80986e8f4b6618a143a046bd5022da510
webstean@gmail.com <(Get-Item Env:UPN).Value>
2026-09-16 18:45:47 +1000

    Remove unused data source and local variables for automation account encryption recommendations

--- subscription-advisor-suppression.tf
+++ subscription-advisor-suppression.tf
@@ -1,22 +1,7 @@
 
 data "azurerm_advisor_recommendations" "all" {}
 
-data "azurerm_advisor_recommendations" "automation_account" {
-  filter_by_category     = ["Security"]
-  filter_by_resource_ids = [azurerm_automation_account.this.id]
-}
-
-locals {
-  automation_account_variable_encryption_recommendations = [
-    for recommendation in data.azurerm_advisor_recommendations.automation_account.recommendations : recommendation
-    if(
-      strcontains(lower(recommendation.recommendation_name), "variable") && strcontains(lower(recommendation.recommendation_name), "encrypt")
-      ) || (
-      strcontains(lower(recommendation.description), "variable") && strcontains(lower(recommendation.description), "encrypt")
-    )
-  ]
-}
-
+/*
 resource "azurerm_advisor_suppression" "automation_account_variables_should_be_encrypted" {
   ## count is 0 when Advisor has not yet surfaced the recommendation (e.g. brand new automation account)
   count             = length(local.automation_account_variable_encryption_recommendations) > 0 ? 1 : 0
@@ -25,8 +10,9 @@ resource "azurerm_advisor_suppression" "automation_account_variables_should_be_e
   resource_id       = azurerm_automation_account.this.id
   ## ttl               = "01:00:00:00" ## omit to suppress indefinitely
 }
+*/
 
-output "advisor_suppression_candidates" {
+output "azure_advisor_suppression_candidates" {
   description = "Advisor recommendations across the subscription that can be used as suppression candidates."
   sensitive   = false
   value = [
